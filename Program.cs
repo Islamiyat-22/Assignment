e64018c64475e0adcb2b42217cfd7f8fbe865f8f
Islamiyat-22 <89584192+Islamiyat-22@users.noreply.github.com>
2021-08-26 08:31:20 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstnumber = 0;
+            int secondnumber = 0;
+            int result = 0;
+            
+            Console.Write("please enter the first number : ");
+            firstnumber = int.Parse(Console.ReadLine());
+
+            Console.Write("please enter the second number : ");
+            secondnumber = int.Parse(Console.ReadLine());
+
+            result = firstnumber / secondnumber ;
+            Console.WriteLine("the answer is: " + result);
+
+        }
+    }
+}
