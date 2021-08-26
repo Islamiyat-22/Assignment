1217a01ccef5412ae46b4630e1de05840edc4e25
Islamiyat-22 <89584192+Islamiyat-22@users.noreply.github.com>
2021-08-26 08:50:25 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Division
+namespace Multiplication
 {
     class Program
     {
@@ -8,6 +8,7 @@ namespace Division
         {
             int firstnumber = 0;
             int secondnumber = 0;
+            int thirdnumber = 0;
             int result = 0;
             
             Console.Write("please enter the first number : ");
@@ -16,9 +17,12 @@ namespace Division
             Console.Write("please enter the second number : ");
             secondnumber = int.Parse(Console.ReadLine());
 
-            result = firstnumber / secondnumber ;
-            Console.WriteLine("the answer is: " + result);
+            Console.Write("please enter the third number : ");
+            thirdnumber = int.Parse(Console.ReadLine());
 
+            result = firstnumber * secondnumber * thirdnumber;
+            Console.WriteLine("the answer is: " + result);
+            
         }
     }
 }
